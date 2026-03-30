65cb159cf942a5b90bb9cbe3ba1bf05917c3a8ab
Truzt CI <devops@zuse.lk>
2026-03-30 09:24:08 +0000

    Update Truzt cask to v2.7.2

--- Casks/truzt.rb
+++ Casks/truzt.rb
@@ -4,8 +4,8 @@ cask "truzt" do
   arch arm: "arm64", intel: "amd64"
 
   version "2.7.2"
-  sha256 arm:   "PLACEHOLDER_ARM64_SHA256",
-         intel: "PLACEHOLDER_AMD64_SHA256"
+  sha256 arm:   "c64030a6d41f9e191c17e44d3c584e743a331e2dd2fea98a6f1b2fc88753af5f",
+         intel: "e9a2700fa5e3ca1dc491b0c948fbdd7ac6c5e1027911977c8f32ffe9e2ee0f06"
 
   url "https://pkgs.truzt.lk/release/v#{version}/Truzt-#{arch}-#{version}.pkg"
   name "Truzt"
@@ -15,11 +15,11 @@ cask "truzt" do
   pkg "Truzt-#{arch}-#{version}.pkg", allow_untrusted: true
 
   uninstall launchctl: "truzt",
-            pkgutil:   "com.zuselk.Truzt",
-            delete:    [
-              "/usr/local/bin/truzt",
-              "/usr/local/bin/truzt-ui",
-            ]
+  pkgutil:   "com.zuselk.Truzt",
+  delete:    [
+    "/usr/local/bin/truzt",
+    "/usr/local/bin/truzt-ui",
+  ]
 
   zap trash: [
     "~/Library/Application Support/Truzt",
