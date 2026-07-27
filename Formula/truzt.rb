1f8c6d5e73154a1e75dfeab8a2058d2b2678c4c7
Truzt CI <devops@zuse.lk>
2026-07-27 10:16:06 +0000

    Update Truzt formula to v2.8.5

--- Formula/truzt.rb
+++ Formula/truzt.rb
@@ -4,31 +4,31 @@
 class Truzt < Formula
   desc "Truzt — secure mesh network client."
   homepage "https://truzt.lk/"
-  version "2.8.4"
+  version "2.8.5"
   license "BSD-3-Clause"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://pkgs.truzt.lk/release/v2.8.4/truzt_2.8.4_darwin_amd64.tar.gz"
-      sha256 "54f0e63afc6d6bf261d364cb8641dfaf72ed5445ca53138d11fdec3ec54a9b9c"
+      url "https://pkgs.truzt.lk/release/v2.8.5/truzt_2.8.5_darwin_amd64.tar.gz"
+      sha256 "5ef9ab9caa6d3554c31052cb0e0b3dbbf6e01d1b1815583c58c803839a5ff785"
       define_method(:install) { bin.install "truzt" }
     end
     if Hardware::CPU.arm?
-      url "https://pkgs.truzt.lk/release/v2.8.4/truzt_2.8.4_darwin_arm64.tar.gz"
-      sha256 "7bf48f7384cb2649733ab3320844ee7f9fd7197dff23940e8b32fa7473110fc7"
+      url "https://pkgs.truzt.lk/release/v2.8.5/truzt_2.8.5_darwin_arm64.tar.gz"
+      sha256 "375354b63de5fb8279e5f279f521515200fd7fee990b4bb06248016f2197eb71"
       define_method(:install) { bin.install "truzt" }
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://pkgs.truzt.lk/release/v2.8.4/truzt_2.8.4_linux_amd64.tar.gz"
-      sha256 "b8f84aacd90d1457ceed85f452aca35d35d9114e111f69c0b4a3bf2592661dd8"
+      url "https://pkgs.truzt.lk/release/v2.8.5/truzt_2.8.5_linux_amd64.tar.gz"
+      sha256 "aff4126e259e87f455551cbc8d6555df1e460aedabd3a9f399dc600f171ce799"
       define_method(:install) { bin.install "truzt" }
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://pkgs.truzt.lk/release/v2.8.4/truzt_2.8.4_linux_arm64.tar.gz"
-      sha256 "d83e7665452d4312af69fc025f27f06aa8311af1bed8486b4318f7b4cbfb0e7f"
+      url "https://pkgs.truzt.lk/release/v2.8.5/truzt_2.8.5_linux_arm64.tar.gz"
+      sha256 "78c48ff65b289fc8cb69258b2b19cc1cbc77e6a1d81015b1c3006d6ba3ae796a"
       define_method(:install) { bin.install "truzt" }
     end
   end
